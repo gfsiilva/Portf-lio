679ab0579824f4f5409e2b8b687fc611220ca9fb
Gustavo Silva <gsiilvac27@gmail.com>
2026-03-31 18:27:42 -0300

    Finalizando estrutura do portfolio e readme

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# Portf-lio-
+# Portfólio
+
+Bem-vindo ao repositório do meu portfolio pessoal. Este projeto foi desenvolvido para demonstrar minhas habilidades unindo o mundo do design visual com a programação full-stack.
+
+## Tecnologias utilizadas
+Design: Figma (Prototipagem, UI/UX e definição de paleta de cores).
+
+Frontend: HTML5, CSS3 e JavaScript (Vanilla JS).
+
+Integrações: API de WhatsApp para contato direto e dinâmico.
+
+Hospedagem: GitHub Pages / Vercel.
+
+### Sobre o projeto
+UI/UX Moderno: Design focado em usabilidade e estética "clean".
+
+Direto ao Ponto: Botão de contato que já inicia uma conversa no meu WhatsApp com os dados preenchidos no formulário.
+
+Performance: Feito com tecnologias nativas para garantir o carregamento mais rápido possível.
+
--- script.js
+++ script.js
@@ -10,3 +10,31 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
   });
 });
+
+
+const formulario = document.querySelector('form');
+
+formulario.addEventListener('submit', function (e) {
+  e.preventDefault();
+  
+  const nome = document.querySelector('input[placeholder="Nome"]').value;
+  const email = document.querySelector('input[placeholder="Email"]').value;
+  const numero = document.querySelector('input[placeholder="Número"]').value;
+  const assunto = document.querySelector('input[placeholder="Assunto"]').value;
+  const mensagem = document.querySelector('textarea').value;
+
+  
+  const meuNumero = "5581998602600"; 
+
+  const textoMensagem = `*Direto do Portfólio*
+    
+*Nome:* ${nome}
+*Email:* ${email}
+*WhatsApp:* ${numero}
+*Assunto:* ${assunto}
+*Mensagem:* ${mensagem}`;
+
+  const linkWhatsApp = `https://api.whatsapp.com/send?phone=${meuNumero}&text=${encodeURIComponent(textoMensagem)}`;
+
+  window.open(linkWhatsApp, '_blank');
+});
